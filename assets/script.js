28fb70a2c66dfece6294f23c0f107616b4ecf341
CurtisCircus <cakrieg63@gmail.com>
2023-10-17 02:00:40 -0400

    I have the fetch API working, and have it set so it will replace text elements with the relavent weather data

--- assets/script.js
+++ assets/script.js
@@ -9,11 +9,42 @@ cityTextarea.addEventListener('keydown', function (event) {
         event.preventDefault();
     }
 });
-
 cityTextarea.addEventListener('keydown', function (event) {
     if (event.key === 'Enter' && cityTextarea.value.trim() !== '') {
         localStorage.setItem('cityValue', cityTextarea.value);
-    }
+
+        city = cityTextarea.value;
+
+        apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+        fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                const temperatureKelvin = data.main.temp;
+                const temperatureFahrenheit = (temperatureKelvin - 273.15) * 9 / 5 + 32;
+                const windSpeed = data.wind.speed;
+                const humidity = data.main.humidity;
+                console.log(
+                    `Current temperature in ${city}: ${temperatureFahrenheit.toFixed(2)}°F`
+                );
+
+                var currentCity = document.getElementById('currentCity'); // Get currentCity element
+                var currentDate = new Date();
+                currentCity.innerHTML = city + ": " + currentDate.toDateString();
+
+                var currentTemp = document.getElementById('currentTemp');
+                currentTemp.innerHTML = "Temp: " + temperatureFahrenheit.toFixed(1) + "°F";
+                var currentWind = document.getElementById('currentWind');
+                currentWind.innerHTML = "Wind: " + windSpeed.toFixed(1) + "MPH"
+                var currentHumidity = document.getElementById('currentHumidity');
+                currentHumidity.innerHTML = "Humidity: " + humidity.toFixed(1) + "%";
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+        }
 });
-city = cityTextarea;
-console.log(cityTextarea);
