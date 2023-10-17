9e0fddf9df1c6c54e70675469b695c1ccedf730b
CurtisCircus <cakrieg63@gmail.com>
2023-10-17 01:07:25 -0400

    I added javascript code to save the textarea value to local storage and uses that value for the city var

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,19 @@
 var APIKey = "d8388590bf9844ca9867ebab3db4127f";
-var city = 'New York'; // Replace with the city you want to get weather data for
+var city = ''; // Replace with the city you want to get weather data for
 var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+
+const cityTextarea = document.getElementById('cityTextarea');
+
+cityTextarea.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault();
+    }
+});
+
+cityTextarea.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter' && cityTextarea.value.trim() !== '') {
+        localStorage.setItem('cityValue', cityTextarea.value);
+    }
+});
+city = cityTextarea;
+console.log(cityTextarea);
