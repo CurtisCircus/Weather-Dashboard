e5f7e3d488289a684e88b5e67a6196a232257e79
CurtisCircus <cakrieg63@gmail.com>
2023-10-16 20:29:05 -0400

    Got the API Key working so I can now use the Weather API

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+var APIKey = "d8388590bf9844ca9867ebab3db4127f";
+var city = 'New York'; // Replace with the city you want to get weather data for
+var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
